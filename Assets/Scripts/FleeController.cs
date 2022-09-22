0506d90aee169dacc2ac74c61f760b9687fe3d4d
Victor <victorfalcon796@gmail.com>
2022-09-22 10:25:12 +0200

    nav mesh

---
+++ Assets/Scripts/FleeController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeController : MonoBehaviour
+{
+    public Transform target;
+    public float acceleration = 1.0f;
+    public float maxVelocity = 10.0f;
+    public float turnAcceleration = 0.2f;
+    public float turnSpeed = 1.0f;
+    public float maxTurnSpeed = 10.0f;
+    public float stopDistance = 2.0f;
+    public float movSpeed;
+
+
+    public Vector3 direction = Vector3.zero;
+    public Vector3 movement = Vector3.zero;
+    public float angle = 0f;
+    public Quaternion rotation = Quaternion.identity;
+    public float freq = 0f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        freq += Time.deltaTime;
+        if (freq < 0.5) return;
+
+        if (Vector3.Distance(target.transform.position, transform.position) <
+         stopDistance) return;
+        Flee();   // calls to this function should be reduced
+        turnSpeed += turnAcceleration * Time.deltaTime;
+        turnSpeed = Mathf.Min(turnSpeed, maxTurnSpeed);
+        movSpeed += acceleration * Time.deltaTime;
+        movSpeed = Mathf.Min(movSpeed, maxVelocity);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
+        transform.position += transform.forward.normalized * movSpeed * Time.deltaTime;
+    }
+    void Flee()
+    {
+        direction = transform.position - target.transform.position;
+        direction.y = 0.0f;
+
+        movement = direction.normalized * maxVelocity;
+        angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
+        rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
+        transform.position += transform.forward.normalized * maxVelocity * Time.deltaTime;
+    }
+}
--- Assets/Scripts/RobberController.cs
+++ Assets/Scripts/RobberController.cs
@@ -1,55 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class RobberController : MonoBehaviour
 {
-    public Transform target;
-    public float acceleration = 1.0f;
-    public float maxVelocity = 10.0f;
-    public float turnAcceleration = 0.2f;
-    public float turnSpeed = 1.0f;
-    public float maxTurnSpeed = 10.0f;
-    public float stopDistance = 2.0f;
-    public float movSpeed;
+    public GameObject target;
+    NavMeshAgent agent;
 
-
-    public Vector3 direction = Vector3.zero;
-    public Vector3 movement = Vector3.zero;
-    public float angle = 0f;
-    public Quaternion rotation = Quaternion.identity;
-    public float freq = 0f;
-    
-    void Start()
+    private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();    
     }
 
-    // Update is called once per frame
     void Update()
     {
-        freq += Time.deltaTime;
-        if (freq < 0.5) return;
-
-        if (Vector3.Distance(target.transform.position, transform.position) <
-         stopDistance) return;
-        Seek();   // calls to this function should be reduced
-        turnSpeed += turnAcceleration * Time.deltaTime;
-        turnSpeed = Mathf.Min(turnSpeed, maxTurnSpeed);
-        movSpeed += acceleration * Time.deltaTime;
-        movSpeed = Mathf.Min(movSpeed, maxVelocity);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
-        transform.position += transform.forward.normalized * movSpeed * Time.deltaTime;
+        Seek();
     }
-    void Seek()
+
+    private void Seek()
     {
-        direction = target.transform.position - transform.position;
-        direction.y = 0.0f;
-
-        movement = direction.normalized * maxVelocity;
-        angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
-        rotation = Quaternion.AngleAxis(angle, Vector3.up);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
-        transform.position += transform.forward.normalized * maxVelocity * Time.deltaTime;
+        agent.destination = target.transform.position;
     }
 }
